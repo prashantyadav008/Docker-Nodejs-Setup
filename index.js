51a768e35113d5a4f518ef64cfce13255f33c273
prashantyadav008 <prashantsdbct009@gmail.com>
2025-03-31 20:27:23 +0530

    Install Basic Packages and Setup Running Node

---
+++ index.js
@@ -0,0 +1,14 @@
+/** @format */
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+const port = 3000;
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);
